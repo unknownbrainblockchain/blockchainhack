c6ed0a1b9989bb5b9537960385df8bb449281d0f
Pavel Sumbaev <p-sumbaev@yandex.ru>
2022-02-06 11:27:25 +0300

    init front

--- peer-node/src/index.ts
+++ peer-node/src/index.ts
@@ -1,17 +1,32 @@
+import express from "express"
 import { Fluence, KeyPair } from "@fluencelabs/fluence";
 import { krasnodar } from "@fluencelabs/fluence-network-environment";
-import { registerGiveMeData, GiveMeDataDef } from "./_aqua/main";
+import {registerGiveMeData, GiveMeDataDef, getFromPeer} from "./_aqua/main";
 import { Listener } from "@listener-js/listener";
-import http from "@listener-js/http";
+import Http from "@listener-js/http";
 import axios from "axios"
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/send', (req, res) => {
+    main();
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+    main();
+})
 
 class GiveMeData implements GiveMeDataDef {
     async getFromPeer() {
-        new Listener("id");
         const url = "127.0.0.1:8080" // поменять адрес и порт
-        const _data = await http.fetch([], url)
+        const _data = await Http.fetch([], url)
 
-        return _data
+        return "str"
     }
 
     async workWithDocker(data: string) {
@@ -19,7 +34,7 @@ class GiveMeData implements GiveMeDataDef {
             data: this.getFromPeer()
           });
 
-        return new_data
+        return "str"
     }
 
     async returnDataToPeer(data: string) {
@@ -29,17 +44,16 @@ class GiveMeData implements GiveMeDataDef {
 
         return "ok"
     }
-
 }
 
-async function main() { 
+
+async function main() {
     console.log("PeerId: ", Fluence.getStatus().peerId);
     console.log("Relay id: ", Fluence.getStatus().relayPeerId);
 
     registerGiveMeData("GiveMeData", new GiveMeData());
-      
+
     console.log("crtl-c to exit");
         // await Fluence.stop();
 }
       
-main();
